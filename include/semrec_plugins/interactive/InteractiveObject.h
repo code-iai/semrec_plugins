7bbca2c91223f7e9a518f87d175d72b56c9ce69d
Jan Winkler <jan.winkler.84@gmail.com>
2016-09-20 21:14:14 +0200

    Fix for license headers

--- include/semrec_plugins/interactive/InteractiveObject.h
+++ include/semrec_plugins/interactive/InteractiveObject.h
@@ -1,4 +1,25 @@
+/*************************************************************************
+ * Copyright (c) 2016, Jan Winkler <jan.winkler.84@gmail.com>.
+ * All rights reserved.
+ * 
+ * This file is part of semrec_plugins.
+ * 
+ * semrec_plugins is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * semrec_plugins is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with semrec_plugins.  If not, see <http://www.gnu.org/licenses/>.
+ *************************************************************************/
+
 /** \author Jan Winkler */
+
 
 #ifndef __INTERACTIVE_OBJECT_H__
 #define __INTERACTIVE_OBJECT_H__
